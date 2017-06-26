9fedbe2cb9cf73b8049362468cfeaf725d0a1177
AFKoala <bubbles.murph@gmail.com>
2017-06-26 11:25:10 -0400

    June 24 Homework

---
+++ addition.rb
@@ -0,0 +1,11 @@
+puts "We're going to add some numbers together!"
+
+puts "Give me a number: "
+num1 = gets.chomp.to_i
+
+puts "Give me another number: "
+num2 = gets.chomp.to_i
+
+answer = num1 + num2
+
+puts "#{num1} + #{num2} = #{answer}"
